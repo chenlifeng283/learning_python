2c39657ac345c25f20c7b5509ff2cca3e6e7fdb8
chenlifeng283 <38682417+chenlifeng283@users.noreply.github.com>
2019-10-25 15:31:07 +0800

    Create code_challenge_solution.py

---
+++ 1-print/code_challenge_solution.py
@@ -0,0 +1,18 @@
+#Here's a challenge for you to help you practice
+#See if you can fix the code below
+
+#There was a single quote inside the string
+#Use double quotes to enclosed the string
+print("Why won't this line of code print.")
+
+#There was a mistake in the function name
+print('this line fails too!')
+
+#Need to add () around the string
+print('I think I know how to fix this one!')
+
+#print the name entered by the user
+#You need to store the value returned by the input satement
+#in a variable
+name=input('What is you name?')
+print(name)
