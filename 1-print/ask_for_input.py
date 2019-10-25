b17d59b43175de999dba2fefab513d9314183705
chenlifeng283 <38682417+chenlifeng283@users.noreply.github.com>
2019-10-25 15:02:33 +0800

    Update ask_for_input.py

--- 1-print/ask_for_input.py
+++ 1-print/ask_for_input.py
@@ -1,5 +1,5 @@
 #The input function allows you to prompy the user for a value
 #You need to declare a variable to hold the value entered by the user
 
-name=input("What's your name?") #if string has single quotes,its must enclosed in double quotes.
+name=input("What's your name?") #if string has single quotes,it must be enclosed in double quotes.
 print(name)
