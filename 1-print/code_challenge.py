7648b74c6ed0aa6418716c29ec25346fff04c17f
chenlifeng283 <38682417+chenlifeng283@users.noreply.github.com>
2019-10-25 15:17:37 +0800

    Create code_challenge.py

---
+++ 1-print/code_challenge.py
@@ -0,0 +1,13 @@
+#Here is a challenge for you to help you to practice
+#See if you can fix the code blew
+
+#print the message
+print('why won't this line of code print')
+
+pirnt('this line fail too!')
+
+print 'I think I know how to fix this!'
+
+#print the name entered by the user
+input('what is your name?')
+print(name)
