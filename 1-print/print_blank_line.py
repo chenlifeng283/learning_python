566276a70559f9dcb2018fd4a2b704246580841f
chenlifeng <38682417+chenlifeng283@users.noreply.github.com>
2019-10-26 08:25:10 +0800

    Create print_blank_line.py

---
+++ 1-print/print_blank_line.py
@@ -0,0 +1,10 @@
+#Each print statement starts on a new line
+print('hello world')
+
+#If you pass nothing to the print statement you get a blank line
+print()
+print('Did you see that blank line')
+
+#'\n' is a special character sequence that means print new line
+#You can use it to break the output over multiple line
+print('blank line \nin the middle of string.')
