836a297e8a8004e22087d747d86f8530205aeb24
chenlifeng <38682417+chenlifeng283@users.noreply.github.com>
2019-10-26 08:48:23 +0800

    Create Comments_for debugging.py

---
+++ Comments/Comments_for debugging.py
@@ -0,0 +1,6 @@
+print('hello world')
+print('It's a small world after all!')
+#If string contains a single qutoes,it must be enclosed in double qutoes.
+#comment it with a hash character(#) when the code executed ingored it
+
+#print('It's a small world after all!')
