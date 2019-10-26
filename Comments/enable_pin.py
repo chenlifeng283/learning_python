d81408bbb6eb55d444eb722156f9dc7b89c1be38
chenlifeng <38682417+chenlifeng283@users.noreply.github.com>
2019-10-26 09:03:12 +0800

    Create enable_pin.py

---
+++ Comments/enable_pin.py
@@ -0,0 +1,16 @@
+#The enable_pin method is not coded yet
+#I have created a dummy method so the code
+#will run without an error
+#Don't panic if you don't understand this part of the code
+#We cover methods in a separate module
+
+def enable_pin(user,pin):
+    pirnt('pin enable')
+    
+#Set current_user and pin to test values  
+current_user='test123'
+pin='123'
+
+#Enable pin check as listed in security requirements
+enable_pin(current_user,pin) #executed this line code can output the "pin enable"
+    
