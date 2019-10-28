0b346b6c9308544b3ec145f19a096f018e9a160c
chenlifeng <38682417+chenlifeng283@users.noreply.github.com>
2019-10-28 13:23:50 +0800

    Create combine_string.py

---
+++ 3-String_variables/combine_string.py
@@ -0,0 +1,7 @@
+# you can use the + operator to concatenate strings
+first_name='ab'
+last_name='cd'
+print(fisrt_name + last_name)
+
+# If you want a space between the strings you must include space within the strings.
+print('hello ' + first_name + ' ' + last_name)
