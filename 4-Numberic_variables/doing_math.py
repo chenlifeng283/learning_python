121472ff9fdf5b6a5bd689f192f04b4bdbcaaf31
chenlifeng <38682417+chenlifeng283@users.noreply.github.com>
2019-10-28 14:34:11 +0800

    Create doing_math.py

---
+++ 4-Numberic_variables/doing_math.py
@@ -0,0 +1,16 @@
+# Because the variables are assigned numberic values when created
+# Python knows there are numberic values
+fisrt_num = 6
+second_num = 2
+
+# You can perform a variety math operation on numberic values
+print('addition')
+print(first_num + second_num)
+print('subtraction')
+print(first_num - second_num)
+print('multiplication')
+print(first_num * second_num)
+print('division')
+print(first_num / second_num)
+print('exponent')
+print(first_num ** second_num)
