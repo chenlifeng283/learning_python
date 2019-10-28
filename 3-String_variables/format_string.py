712ade748745ff5966dbf4cd886fc58759cd6911
chenlifeng <38682417+chenlifeng283@users.noreply.github.com>
2019-10-28 13:30:01 +0800

    Create format_string.py

---
+++ 3-String_variables/format_string.py
@@ -0,0 +1,6 @@
+# Ask for the user their first and last name.
+first_name = input('what is your fisrt name?')
+last_name = input('what is your last name?')
+
+# The capitalize function will return the string with the first lettle uppercase and rest of word lowercase.
+print('hello ' + fisrt_name.capitalize() + ' ' + last_name.capitalize())
