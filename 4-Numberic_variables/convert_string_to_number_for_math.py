ae396de1ec1ea9e343d246697fe6933c559c0295
chenlifeng <38682417+chenlifeng283@users.noreply.github.com>
2019-10-28 14:41:52 +0800

    Create convert_string_to_number_for_math.py

---
+++ 4-Numberic_variables/convert_string_to_number_for_math.py
@@ -0,0 +1,10 @@
+first_num = input('enter fisrt num')
+second_num = input('enter second num')
+# If you have a string variable containing number
+# And you want treat it as a number
+# You must convert it to a numberic datatype
+# int() converts a string to an integer e.g. 4, 5, 100
+print(int(first_num) + int(second_num))
+
+# float() converts a string to a decimal or float e.g. 1.0, 2.3, 3.1415
+print(float(first_num) + float(second_num))
