871a600da6c59eb508614aff6b1f8fe31caf4342
chenlifeng <38682417+chenlifeng283@users.noreply.github.com>
2019-10-29 09:49:22 +0800

    Create code_challenge_and_solution.py

---
+++ 4-Numberic_variables/code_challenge_and_solution.py
@@ -0,0 +1,17 @@
+# Ask a user enter a number
+first_num = input('enter a number')
+
+# Ask a user enter a second number
+second_num=input('enter a second number')
+
+# Calculate the total of the two numbers added together
+anser=float(first_num) + float(second_num)
+
+# Print 'fisrt number + second number =answer'
+# For example if someone and 4 and 6 the output should read 4 + 6 = 10
+print(first_num + '+' + second_num + '=' + str(anwser))
+
+# If you do not want decimal places you can round the answer
+print(fisrt_num + '+' + sencond_num + '=' + str(round(answer)))
+
+
