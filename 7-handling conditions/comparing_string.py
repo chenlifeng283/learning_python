c0b12f85032743a96c45829b974078248a379faa
chenlifeng <38682417+chenlifeng283@users.noreply.github.com>
2019-10-30 13:43:54 +0800

    Create comparing_string.py

---
+++ 7-handling conditions/comparing_string.py
@@ -0,0 +1,7 @@
+country = input('where are come from?')
+if country == 'canada':
+    #string comparision are case insensitive 
+    #if you typed in CANADA or Canada  it will not match
+    print('So you must like hockey!')
+else:
+    print('You are not from Canada!)
