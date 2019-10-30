e836ca0673d990c45dd30386861924ac0258a337
chenlifeng <38682417+chenlifeng283@users.noreply.github.com>
2019-10-30 13:24:26 +0800

    Create add_else_different_indentation.py

---
+++ 7-handling conditions/add_else_different_indentation.py
@@ -0,0 +1,9 @@
+price = 5.0
+if price >= 1.0:
+    tax = 0.07
+else:
+    tax = 0
+   
+# The print statement below is not indented so is executed after the 
+# if statement is evaluated
+print(tax)
