17e7a953d4e6a3e9926d17694ebbd6fc52addfa3
chenlifeng <38682417+chenlifeng283@users.noreply.github.com>
2020-03-03 15:33:06 +0800

    Create 多页面情况.py

---
+++ spider/多页面情况.py
@@ -0,0 +1,10 @@
+第一页  https://movie.douban.com/top250
+第二页  https://movie.douban.com/top250?start=25&filter=
+第三页  https://movie.douban.com/top250?start=50&filter=
+第四页  https://movie.douban.com/top250?start=75&filter=
+
+for i in range(10):
+        url = 'https://movie.douban.com/top250?start={}&filter='.format(i * 25)
+        
+    
+    
