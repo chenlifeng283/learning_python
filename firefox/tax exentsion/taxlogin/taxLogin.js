02fabffc9b7abda701731249aec96c1671931a67
chenlifeng <38682417+chenlifeng283@users.noreply.github.com>
2020-08-19 16:20:03 +0800

    Add files via upload

---
+++ firefox/tax exentsion/taxlogin/taxLogin.js
@@ -0,0 +1,21 @@
+// document.body.textContent = "";
+
+// var header = document.createElement('h1');
+// header.textContent = "This page has been eaten";
+// document.body.appendChild(header);
+
+// document.body.style.border = "5px solid red"
+
+// document.getElementById("bsysf").click();
+// var name = document.getElementById("bsryyz").innerText
+// alert(name)
+
+
+	$("#bsysf").click();
+	var a = $("tr:contains('陈利锋')");
+	a.click();
+	var b = $("label");
+	b[3].click();
+	$("#yz-mm")[0].value = 'a123456';
+	$("#yz-login")[0].click();
+
